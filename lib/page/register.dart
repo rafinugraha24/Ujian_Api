71ebd21771863bb2db4b9125f682b7c6480838a2
rafinugraha24 <rafinugraha.lp3i.krw24@gmail.com>
2025-12-11 19:23:36 +0700

    statefull

--- lib/page/register.dart
+++ lib/page/register.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class register extends StatefulWidget {
+  const register({super.key});
 
   @override
+  State<register> createState() => _registerState();
+}
+
+class _registerState extends State<register> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
